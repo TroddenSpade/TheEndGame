3e39a73aba2ec25fc71e7f6a60b3478785bb2f76
TroddenSpade <parsasam1378@gmail.com>
2019-06-05 20:28:38 +0430

    nextColor method added

--- src/towerRoyal/map/Tile.java
+++ src/towerRoyal/map/Tile.java
@@ -1,36 +1,30 @@
 package towerRoyal.map;
 
-import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import towerRoyal.soldiers.Soldier;
 
-enum Type {
-    GREY(Color.GREY),
-    BLUE(Color.BLUE),
-    YELLOW(Color.YELLOW),
-    RED(Color.RED),
-    GREEN(Color.GREEN);
+import java.util.ArrayList;
 
-    private Color color;
 
-    Type(Color color){
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-}
-
-public class Tile {
+public class Tile extends Rectangle{
     private Type type;
+    private ArrayList<Soldier> mySoldiers = new ArrayList<>();
+    private ArrayList<Soldier> oppoSoldiers = new ArrayList<>();
 
-    public Tile(Type type){
+    public Tile(Type type,int x,int y,int height){
+        super(x,y,height,height);
         this.type = type;
+        this.setFill(type.getColor());
+        this.setStroke(Color.BLACK);
     }
 
-    public Rectangle getTile(){
-        Rectangle rectangle = new Rectangle(20,20);
-        rectangle.setStroke(type.getColor());
-        return rectangle;
+    public void nextColor(){
+        this.type = Type.values()[(this.type.ordinal()+1)%5];
+        this.setFill(type.getColor());
+    }
+
+    public Type getType() {
+        return type;
     }
 }
