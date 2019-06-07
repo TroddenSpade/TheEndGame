b30d4ab5f73f7b4f489f6685b98a68fa1b2f2386
TroddenSpade <parsasam1378@gmail.com>
2019-06-08 02:34:47 +0430

    tower Field added

--- src/towerRoyal/map/Tile.java
+++ src/towerRoyal/map/Tile.java
@@ -2,13 +2,16 @@ package towerRoyal.map;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import towerRoyal.Main;
 import towerRoyal.soldiers.Soldier;
+import towerRoyal.towers.Tower;
 
 import java.util.ArrayList;
 
 
 public class Tile extends Rectangle{
     private Type type;
+    private Tower tower;
     private ArrayList<Soldier> mySoldiers = new ArrayList<>();
     private ArrayList<Soldier> oppoSoldiers = new ArrayList<>();
 
@@ -24,7 +27,18 @@ public class Tile extends Rectangle{
         this.setFill(type.getColor());
     }
 
+    public void setTower(Tower tower){
+        this.tower = tower;
+    }
+
     public Type getType() {
         return type;
+    }
+
+    public boolean isEmpty(){
+        if(tower == null){
+            return true;
+        }
+        return false;
     }
 }
