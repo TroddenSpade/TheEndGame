4797ce6f59e82831d654e08c7c6ef8f1b2a08e0e
TroddenSpade <parsasam1378@gmail.com>
2019-06-11 22:46:45 +0430

    monitoredGroupForSettingSoldiers method added

--- src/towerRoyal/map/Map.java
+++ src/towerRoyal/map/Map.java
@@ -3,10 +3,14 @@ package towerRoyal.map;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
 import towerRoyal.Main;
 import towerRoyal.Player;
+import towerRoyal.soldiers.Soldier;
 import towerRoyal.towers.Tower;
 
 import java.io.File;
@@ -18,6 +22,7 @@ import java.io.OutputStreamWriter;
 
 public class Map{
     private Tile[][] map;
+    private int[][] intMap;
     private int length;
     private int tileHeight;
     private Group group = new Group();
@@ -25,17 +30,15 @@ public class Map{
 
     public Map(int[][] table,int length){
         this.length = length;
+        this.intMap = table;
         this.map = new Tile[length][length];
         this.tileHeight = (WIDTH/length);
         this.group.setLayoutX(tileHeight*length);
         this.group.setLayoutX(tileHeight*length);
         for(int i=0; i<length; i++){
             for(int j=0; j<length; j++){
-                map[i][j] = new Tile(
-                        Type.values()[table[i][j]],
-                        j*((int)WIDTH/length),
-                        i*((int)WIDTH/length),
-                        ((int)WIDTH/length));
+                map[i][j] = new Tile(Type.values()[table[i][j]],j,i,
+                        WIDTH/length,this,length);
                 group.getChildren().add(map[i][j]);
             }
         }
@@ -47,11 +50,8 @@ public class Map{
         this.map = new Tile[length][length];
         for(int i=0; i<length; i++){
             for(int j=0; j<length; j++){
-                map[i][j] = new Tile(
-                        Type.values()[0],
-                        ((int)WIDTH/length)*j,
-                        ((int)WIDTH/length)*i,
-                        ((int)WIDTH/length));
+                map[i][j] = new Tile(Type.values()[0],j,i,
+                        WIDTH/length);
                 group.getChildren().add(map[i][j]);
             }
         }
@@ -111,17 +111,29 @@ public class Map{
                         p.pickedBuilder();
                     }
                     tile.setTower(tower);
-                    tower.setTile(tile);
+                    tower.setTile(tile,this);
                     p.removeTowerFromList(tower);
+                    (new Thread(tower)).start();
                     Main.setSelectedPlayer(null);
                     Main.setSelectedTower(null);
-                    ImageView imv = tower.getImageView();
-                    imv.setLayoutX(tile.getX());
-                    imv.setLayoutY(tile.getY());
-                    imv.setFitHeight(tileHeight);
-                    imv.setFitWidth(tileHeight);
-                    group.getChildren().add(imv);
+                    group.getChildren().add(tower.getTowerPane(tile));
+                }
+            }
+        });
+        return group;
+    }
 
+    public Group monitoredGroupForSettingSoldiers() {
+        this.group.setOnMouseClicked(event -> {
+            Soldier soldier = Main.getSelectedSoldier();
+            Player p = Main.getSelectedPlayer();
+            Tile tile = this.map[(int)event.getY()/tileHeight][(int)event.getX()/tileHeight];
+            if(soldier != null && !tile.hasSoldier()){
+                if(tile.getType().equals(Type.RED)){
+                    soldier.setTile(tile,this);
+                    group.getChildren().add(soldier.getSoldierPane());
+                    (new Thread(soldier)).start();
+                    Main.setSelectedSoldier(null);
                 }
             }
         });
@@ -129,11 +141,23 @@ public class Map{
     }
 
 
+    public void removeFromPane(BorderPane obj){
+        group.getChildren().remove(obj);
+    }
+
     public Group getGroup() {
         return group;
     }
 
     public int getLength() {
         return length;
+    }
+
+    public int[][] getIntMap() {
+        return intMap;
+    }
+
+    public Tile[][] getMap() {
+        return map;
     }
 }
