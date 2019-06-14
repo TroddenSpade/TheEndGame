b724ac26693be588a675e7ee5c3dc86b9ef75c21
TroddenSpade <parsasam1378@gmail.com>
2019-06-14 11:50:46 +0430

    progress bar style

---
+++ src/towerRoyal/soldiers/Knifer.java
@@ -0,0 +1,9 @@
+package towerRoyal.soldiers;
+
+import towerRoyal.Player;
+
+public class Knifer extends Soldier{
+    public Knifer(Player owner){
+        super(owner,"Knifer",5,10,2,1,1,Type.MELEE);
+    }
+}
